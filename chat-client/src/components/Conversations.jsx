5cc0483777b6249c65b1226cf687e612af805a02
Evan Moore <etmoore@gmail.com>
2017-07-31 11:28:37 -0400

    Added chat windows to Conversations component
    Hard-coded everything. Will break chat windows into separate components later.

--- chat-client/src/components/Conversations.jsx
+++ chat-client/src/components/Conversations.jsx
@@ -5,7 +5,32 @@ class Conversations extends Component {
   render () {
     return (
       <div className='Conversations'>
-        Here are your active chats!
+
+        <div className='chat'>
+          <div className='chat-header'>
+            <h2>PoShen</h2>
+          </div>
+          <div className='chat-body'>
+            <p><strong>WillC:</strong> Hey Po!</p>
+            <p><strong>PoShen: </strong> Hey Will, what's up?</p>
+          </div>
+          <div className='chat-input'>
+            <input type='text' name='message' />
+          </div>
+        </div>
+
+        <div className='chat'>
+          <div className='chat-header'>
+            <h2>Tom</h2>
+          </div>
+          <div className='chat-body'>
+            <p><strong>Tom:</strong> Hey Will, fix the the site!</p>
+            <p><strong>WillC: </strong> Sorry Tom...</p>
+          </div>
+          <div className='chat-input'>
+            <input type='text' name='message' />
+          </div>
+        </div>
       </div>
     )
   }
