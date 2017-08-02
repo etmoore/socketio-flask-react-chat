8b386ce566d1ef1925b49af945f4e864826040c5
Evan Moore <etmoore@gmail.com>
2017-08-02 16:31:45 -0400

    Emit and print message#send and message#response

--- chat-client/src/App.js
+++ chat-client/src/App.js
@@ -5,27 +5,52 @@ import Conversations from './components/Conversations'
 import io from 'socket.io-client'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { message: '' }
+    this.socket = io('http://localhost:5000')
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange (event) {
+    this.setState({ message: event.target.value })
+  }
+  handleSubmit (event) {
+    event.preventDefault()
+    this.socket.emit('message#send', {message: this.state.message})
+  }
+  componentDidMount () {
+    this.socket.on('connect', () => {
+      console.log('Client connected!')
+    })
+    this.socket.on('message#receive', (data) => {
+      const textNode = document.createTextNode(data.message)
+      document.getElementById('log').appendChild(textNode)
+    })
+    this.socket.on('my_response', (data) => {
+      console.log(data.message)
+    })
+  }
   render () {
     return (
       <div className='App'>
         <h1>Chat Server</h1>
         <ControlBar />
         <Conversations />
-        <div className='log' />
+        <form action='#' method='POST' onSubmit={this.handleSubmit}>
+          <label>
+            Message:
+            <input type="text"
+                   onChange={this.handleChange}
+                   value={this.state.message} />
+          </label>
+          <input type="submit" value="Send" />
+        </form>
+        <div id='log' />
       </div>
     )
   }
 
-  componentDidMount () {
-    const socket = io('http://localhost:5000')
-
-    socket.on('connect', () => {
-      console.log('Client connected!')
-    })
-    socket.on('my_response', (data) => {
-      console.log(data.message)
-    })
-  }
 }
 
 export default App
--- server/server/server.py
+++ server/server/server.py
@@ -4,10 +4,16 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'development key'
-socketio = SocketIO(app)
+socket = SocketIO(app)
 CORS(app)
 
 
-@socketio.on('connect')
+@socket.on('connect')
 def connect():
-    emit('my_response', {'message': 'Hello from the server :)'})
+    emit('my_response', {'message': 'Hello from the server'})
+
+
+@socket.on('message#send')
+def message_send(data):
+    print(data)
+    emit('message#receive', {'message': data['message']})
