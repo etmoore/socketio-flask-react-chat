7d1e6575a74d23cf395d13f55027330461848b22
Evan Moore <etmoore@gmail.com>
2017-08-07 12:24:46 -0400

    Only install node_modules if they don't exist

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 cd client
-npm install
-npm build
+if [ ! -d "./node_modules" ]; then
+  npm install
+fi
+npm run-script build
 
 cd ../server
-pip install --editable .
+if [ -x "$(command -v pip3)" ]; then # if pip3 executable exists in PATH
+  pip3 install --editable .
+else
+  pip install --editable .
+fi
 export FLASK_APP=server
+export FLASK_DEBUG=true
 flask run
