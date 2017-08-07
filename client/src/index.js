f8c278c3dc02110eb7b06b615af33589568f1fdc
Evan Moore <etmoore@gmail.com>
2017-08-06 22:23:30 -0400

    Delete service worker generated by create-react-app

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
 
 import io from 'socket.io-client'
 const socket = io('http://localhost:5000')
 
 ReactDOM.render(<App socket={socket} />, document.getElementById('root'))
-registerServiceWorker()
